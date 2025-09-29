e40a8830efa1636b8609f41f0a06a4fdf8830113
Muaz-Ibn-Kamal <muazibnkamal8@gmail.com>
2025-09-30 00:25:25 +0600

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+const nextConfig = {};
 
-<<<<<<< HEAD
-export default nextConfig
-=======
-export default nextConfig
->>>>>>> 348ff9f957c6eb026c815687734db1a3d7ab6a4b
+export default nextConfig;
